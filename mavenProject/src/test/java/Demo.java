bc17d219f235a6a4946956bbde661eca27f9c42a
meghadeshmukh896-bit <meghadeshmukh896@gmail.com>
2025-12-16 22:36:31 +0530

    Added report class

---
+++ mavenProject/src/test/java/Demo.java
@@ -0,0 +1,12 @@
+import dynamicCode.ParentClass;
+
+public class Demo extends ParentClass {
+
+	public static void main(String[] args) {
+
+		launch_browser("chrome");
+
+		Hiturl("https://myntra.com");
+	}
+
+}
---
+++ mavenProject/src/test/java/Reports/ReportDemo1.java
@@ -0,0 +1,50 @@
+package Reports;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import com.aventstack.extentreports.reporter.configuration.Theme;
+
+import dynamicCode.ParentClass;
+
+public class ReportDemo1 extends ParentClass {
+
+	public static void main(String[] args) {
+		String projectPath = System.getProperty("user.dir");
+		ExtentSparkReporter esr = new ExtentSparkReporter(projectPath + "\\ReportsFile\\ReportGenrated\\");
+		esr.config().setDocumentTitle("Automation Reports");
+		esr.config().setReportName("Ecommers Reports");
+		esr.config().setTheme(Theme.DARK);
+
+		ExtentReports er = new ExtentReports();
+		er.attachReporter(esr);
+
+		/// Syetem.configuration
+		er.setSystemInfo("os", "Window");
+		er.setSystemInfo("browser", "chrome");
+		er.setSystemInfo("Domain", "Ecommers");
+		er.setSystemInfo("Application", "myntra");
+		er.setSystemInfo("Quality Analyst", "Divayni");
+		er.setSystemInfo("os", "Window");
+
+		/// Create TestCase
+		ExtentTest et = er.createTest("Launch Browser");
+		et.log(Status.INFO, "LaunchBrowser");
+
+		ExtentTest et1 = er.createTest("Hit URL");
+		et1.log(Status.PASS, "Pass Hit URL");
+
+		ExtentTest et2 = er.createTest("Search Functionality");
+		et2.log(Status.FAIL, "Fail Functionality");
+
+		ExtentTest et3 = er.createTest("Send Data");
+		et3.log(Status.SKIP, "Skip Send Data");
+
+		launch_browser("chrome");
+
+		Hiturl("https://myntra.com");
+		er.flush();
+	}
+
+}
