f6bd3f60a73c76df81f3cef704f287079f884543
meghadeshmukh896-bit <meghadeshmukh896@gmail.com>
2025-12-19 10:18:53 +0530

    Add TestNG Project

---
+++ TestNGProject/src/main/java/TestNG1/TestNG1/App.java
@@ -0,0 +1,13 @@
+package TestNG1.TestNG1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ TestNGProject/src/test/java/TestNGone/NewTestone.java
@@ -0,0 +1,9 @@
+package TestNGone;
+
+import org.testng.annotations.Test;
+
+public class NewTestone {
+  @Test
+  public void f() {
+  }
+}
